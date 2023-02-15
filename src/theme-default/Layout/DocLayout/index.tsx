ce7f06957c49d8c8ee7743356294caafd918ce52
caijianhao <caijianhao@yy.com>
2023-02-15 14:37:29 +0800

    feat: add toc aside

--- src/theme-default/Layout/DocLayout/index.tsx
+++ src/theme-default/Layout/DocLayout/index.tsx
@@ -1,11 +1,12 @@
 import { usePageData, Content } from '@runtime';
 import { useLocation } from 'react-router-dom';
+import { Aside } from '../../components/Aside';
 import { DocFooter } from '../../components/DocFooter';
 import { Sidebar } from '../../components/Sidebar';
 import styles from './index.module.scss';
 
 export function DocLayout() {
-  const { siteData } = usePageData();
+  const { siteData, toc } = usePageData();
   const sidebarData = siteData.themeConfig?.sidebar || {};
   const { pathname } = useLocation();
   const matchedSidebarKey = Object.keys(sidebarData).find((key) => {
@@ -19,12 +20,15 @@ export function DocLayout() {
   return (
     <div>
       <Sidebar sidebarData={matchedSidebar} pathname={pathname} />
-      <div className={styles.content}>
-        <div>
+      <div className={styles.content} flex="~">
+        <div className={styles.docContent}>
           <div className="island-doc">
             <Content />
           </div>
           <DocFooter />
+        </div>
+        <div className={styles.asideContainer}>
+          <Aside headers={toc} />
         </div>
       </div>
     </div>
